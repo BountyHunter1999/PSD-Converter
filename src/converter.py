6f1fea9bdd9dc41351f6a247b7f0837946dd667d
mikeyy2 <adhikari.shrish1@gmail.com>
2023-01-20 17:16:43 +0545

    Updated: multi layer groups consideration

--- src/converter.py
+++ src/converter.py
@@ -1,32 +1,54 @@
 from psd_tools import PSDImage
 
+import os
+import argparse
 
-filepath = "data/fahrenheit fridays 7.psd"
-filename = filepath.split(".")[0]
-psd = PSDImage.open(filepath)
-# psd.composite().save(filename.split(".")[0]+".png")
+# filepath = "data/fahrenheit fridays 7.psd"
+filepath = "data/fahrenheit fridays 8.psd"
 
-# Access the layers metadata
-layers = psd._layers
 
 # Print the layers metadata
 # print(layers)
 
-# Iterate through the layers
-for layer in layers:
-    # Print the layer name and visibility status
-    # print(f"Layer Name: {layer.name}, Visible: {layer.visible}")
-    
-    # Check if the layer has text
-    for nested_layer in layer._layers:
-        if nested_layer.kind == "type":
+
+
+def get_layer_data(layers):
+    # Iterate through the layers
+    for layer in layers:
+        # Print the layer name and visibility status
+        # print(f"Layer Name: {layer.name}, Visible: {layer.visible}")
+        
+        # Check if the layer has text
+        # print("Layer Name: ", layer.name)
+        if layer.kind == "type":
             # Extract the text
-            text = nested_layer.text
-            print(f"Layer Name: {nested_layer.name}, Text: {nested_layer.text}")
-    
-    print()
+            text = layer.text
+            print(f"> Layer Name: {layer.name}, Text: {text}")
+        if layer.kind == "group":
+
+            if layers_ := layer._layers:
+                get_layer_data(layers_)
+                print()
+            
 
 # for layer in psd:
 #     print(layer)
 #     layer_image = layer.composite()
 #     layer_image.save("%s/%s.png" % (filename, layer.name))
+
+def get_filepath():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help="The filepath to the file")
+    args = parser.parse_args()
+    return args.filepath
+
+if __name__ == "__main__":
+    # filepath = get_filepath()
+    filename = filepath.split(".")[0]
+    psd = PSDImage.open(filepath)
+    # psd.composite().save(filename.split(".")[0]+".png")
+
+    # Access the layers metadata
+    layers = psd._layers
+    get_layer_data(layers)
+    
